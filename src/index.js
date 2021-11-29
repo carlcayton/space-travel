a900efb17a4bf51d15efa20d78417acbdfde90f3
Arian Cayton <ariancayton@gmail.com>
2021-11-29 10:14:03 +0800

    fix: responsive destination shots

--- src/index.js
+++ src/index.js
@@ -7,17 +7,24 @@ $(document).ready(function () {
             $(`#dest-shot-${i}`)
                .find(`.destination-shot.shot${j}`)
                .css({
+
                   'background-image': `url(../assets/images/destination-shots/dest-shot-${i}/shot${j}.jpg)`,
                   // 'background-color': 'Green',
+                  'padding-top': `calc(${j === 1 ? 30 : 15}% *(4/4))`,
+                  'padding-left': `calc(${j === 1 ? 30 : 15}% *(4/4))`,
                   'border-radius': '50%',
                   'background-size': 'cover',
-                  'width': `${j === 1 ? 20 : 10}vw`,
-                  'height': `${j === 1 ? 20 : 35}vh`,
+                  'width': `${j === 1 ? 30 : 10}%`,
+                  'height': `${j === 1 ? 50 : 25}`,
+                  // 'flex': '1',
+                  // 'width': '50%',
+                  // 'height': '100%',
                   'position': 'absolute',
                   'margin': '0.5vw auto',
-                  'top': 0,
-                  'left': `${j * 100}px`,
+                  // 'top': 0,
+                  'left': `${j * 150}px`,
                   'box-shadow': '0 8px 8px -1px lightblue',
+
                })
          }
       }

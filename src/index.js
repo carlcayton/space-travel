00028f1beb3632af669f3f36cb5db0ac670e734f
Arian Cayton <ariancayton@gmail.com>
2021-11-30 10:24:54 +0800

    refactor: stylings and jquery

--- src/index.js
+++ src/index.js
@@ -9,23 +9,21 @@ $(document).ready(function () {
                   .find(`.destination-shot.shot${j}`)
                   .addClass('is-last-hovered')
             }
-
-
             $(`#dest-shot-${i}`)
                .find(`.destination-shot.shot${j}`)
                .css({
                   'background-image': `url(../assets/images/destination-shots/dest-shot-${i}/shot${j}.jpg)`,
                   // 'background-color': 'Green',
-                  'padding-top': `calc(${j === 1 ? 25 : 5}% *(4/4))`,
-                  'padding-left': `calc(${j === 1 ? 25 : 5}% *(4/4))`,
+                  'padding-top': `calc(${j === 1 ? 25 : 5}% )`,
+                  'padding-left': `calc(${j === 1 ? 25 : 5}%)`,
                   'border-radius': '50%',
                   'background-size': 'cover',
                   'position': 'absolute',
                   'display': 'flex',
                   'margin-bottom': '10%',
-                  'left': `${j * 80}px`,
+                  'margin-left': '10%',
+                  'left': `${j * 2}%`,
                   'box-shadow': '0 8px 8px -1px lightblue',
-
                })
          }
       }
@@ -55,7 +53,28 @@ $(document).ready(function () {
       }
    }
 
+   const JSON_PATH = '../assets/data/info.json'
+   function insertInfo() {
+      $.getJSON(JSON_PATH, function (data) {
+         const allDestinationData = data.destinationData;
+         for (let i = 0; i < allDestinationData.length; i++) {
+            let counter = 0;
+            const destinationData = allDestinationData[i]
+            //Traverse through the object using keys to access the value later
+            for (var key in destinationData) {
+               if (key === 'id') continue
+               $(`#destination-info-${i}`).children().eq(counter).text(
+                  //Append Distance from earth if the current counter is at index 2
+                  `${counter == 2 ? 'Distance from Earth: ' : ''} ${destinationData[key]}`
+               )
+               counter++;
+            }
+         }
+      })
+   }
+
    insertImagesToContainers()
    addHoverEffectToImages();
+   insertInfo();
 
 })
